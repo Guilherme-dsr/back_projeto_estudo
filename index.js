deddc73b77382fbdd7fffd66f36d2c9c8c08c9c3
Guilherme <guilhermesilva.dsr@gmail.com>
2022-02-17 19:50:20 -0300

    Atualizando o endpoint do Create

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
-const Person = require('./models/Person');
-
 // forma de ler JSON / middlewares
 app.use(express.urlencoded({
      extended: true
@@ -14,17 +12,19 @@ app.use(express.urlencoded({
 
 app.use(express.json())
 
+// Rotas da aplicação
+const personRoutes = require('./routes/personRoutes');
+app.use("/person", personRoutes)
+
 // Rota  inicial / endpoinnt 
 app.get('/', (req, res) => {
     res.json({ message: 'Oi Express!'})
 })
-
 /// Entregar porta
 const DB_USER = "Guilherme"
 const DB_PASSWORD = encodeURIComponent("-q$7e_V-@r-X64g")
 
-
-mongoose
+    mongoose
     .connect(
         `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.cymoa.mongodb.net/bancodaapi?retryWrites=true&w=majority`
         )
---
+++ routes/personRoutes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router()
+const Person = require('../models/Person');
+
+router.post('/', async(req, res) => {
+
+    //req.body  
+    const {name, salary, aproved} = req.body
+
+    if(!name){
+        res.status(422).json({ error: 'O nome é obrigaatorio!' })
+    }
+    
+    const person = {
+        name,
+        salary,
+        aproved,
+    }
+
+    try {
+        
+        await Person.create(person)
+
+        res.status(201).json({ message: 'Pessoa inserida com sucesso!' })
+
+    } catch (error) {
+        res.status(500).json({ error: error })
+    }
+
+})
+
+module.exports = router
