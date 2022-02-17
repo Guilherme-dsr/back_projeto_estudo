7c6e0264e2ffa66469eec37da5b6612bd098b5ca
Guilherme <guilhermesilva.dsr@gmail.com>
2022-02-16 21:24:11 -0300

    Configurando prorjeto e banco mongo DB

---
+++ index.js
@@ -0,0 +1,37 @@
+// -q$7e_V-@r-X64g
+// Config inicial
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+const Person = require('./models/Person');
+
+// forma de ler JSON / middlewares
+app.use(express.urlencoded({
+     extended: true
+    }), 
+)
+
+app.use(express.json())
+
+// Rota  inicial / endpoinnt 
+app.get('/', (req, res) => {
+    res.json({ message: 'Oi Express!'})
+})
+
+/// Entregar porta
+const DB_USER = "Guilherme"
+const DB_PASSWORD = encodeURIComponent("-q$7e_V-@r-X64g")
+
+
+mongoose
+    .connect(
+        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.cymoa.mongodb.net/bancodaapi?retryWrites=true&w=majority`
+        )
+    .then(() => {
+        console.log("Conectamos ao MongoDB!")
+        app.listen(3000)
+    })
+    .catch((err) => console.log(err))
+
+
